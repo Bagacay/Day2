9c244c9e52eb676856d1f6de5b2df839529ada66
Heisei <92716223+Bagacay@users.noreply.github.com>
2023-06-09 22:51:31 +0800

    second day
    did html css and a little bit of js. tried to do grid but went with flexbox instead.

---
+++ index.js
@@ -0,0 +1,8 @@
+function navBar() {
+    var nav = document.querySelector("nav");
+    if (nav.style.display === "block") {
+      nav.style.display = "none";
+    } else {
+      nav.style.display = "block";
+    }
+  }
